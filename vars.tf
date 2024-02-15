fab1500fbc7f1da48237de75cdef4b03ee227701
Hari Sriya Pelluri <pelluri.h@northeastern.edu>
2024-02-15 10:53:45 -0500

    Added Project and config

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "The GCP project ID"
+}
+
+variable "region" {
+  description = "The GCP region where resources will be created"
+  default     = "us-east1"
+}
+
